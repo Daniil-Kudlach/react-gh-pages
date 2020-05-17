ebd63ba4232494dc53503bce2b607059e2539ca3
Daniil-Kudlach <daniil.kudlach2018@gmail.com>
2020-05-17 15:38:07 +0300

    Updates

---
+++ precache-manifest.3aba884767feb12803949bbbffec05bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89fcbf122f6abc916dd4fb07bbad25fb",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "455a05bf9a5873dbb156",
+    "url": "/react-gh-pages/static/css/main.c4aee2d5.chunk.css"
+  },
+  {
+    "revision": "6f8ab727523fb172009e",
+    "url": "/react-gh-pages/static/js/2.7d998b8d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-gh-pages/static/js/2.7d998b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "455a05bf9a5873dbb156",
+    "url": "/react-gh-pages/static/js/main.042b5c31.chunk.js"
+  },
+  {
+    "revision": "9d34abfb51a80631acf8",
+    "url": "/react-gh-pages/static/js/runtime-main.fd22a710.js"
+  }
+]);
